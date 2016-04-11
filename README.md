4cc427a3cdf061a94ac15dd916a40b95a9738f31
alto <alto@abas.de>
2016-04-11 17:08:17 +0200

    Initial commit for VARNAMELIST

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # abas-tools-showcase
 Showcase for all modules in abas tools
+
+
+The showcase contains an infosystem which shows the english/german names of all fields in a Vartab. The goal of the showcase is to show the integration of different tools and how to use them together in abas tools.
+
+* AJO Developer
+* FO Developer
+* Screen Designer
+* Report Designer
+
+
+Installation
+
+1. Make sure you have the workDir ow1 in abas
+2. Import the infosystem from the project showcase/infosystem
+2.1 copy the tgz file to the root folder of client
+2.2 extract the tgz file using tar -xf option
+2.3 Import the infosystem using abas ERP and then generate the Java class
+3. Import the project into client/java and add java/projects/showcase/classes to mandant.classpath
+
---
+++ showcase/src/de/abas/training/infosystem/eventhandler/ControlVarnameListEventHandler.java
@@ -0,0 +1,101 @@
+package de.abas.training.infosystem.eventhandler;
+
+import de.abas.erp.jfop.rt.api.annotation.RunFopWith;
+import de.abas.erp.axi2.event.ButtonEvent;
+import de.abas.erp.axi2.annotation.EventHandler;
+import de.abas.erp.axi.event.EventException;
+import de.abas.erp.axi2.event.FieldEvent;
+import de.abas.erp.axi.screen.ScreenControl;
+import de.abas.erp.db.infosystem.custom.ow1.ControlVarnameList;
+import de.abas.erp.db.schema.company.Vartab;
+import de.abas.erp.db.schema.company.Vartab.Row;
+import de.abas.erp.axi2.annotation.ButtonEventHandler;
+import de.abas.erp.axi2.EventHandlerRunner;
+import de.abas.erp.axi2.type.FieldEventType;
+import de.abas.erp.axi2.annotation.FieldEventHandler;
+import de.abas.erp.db.DbContext;
+import de.abas.erp.axi2.type.ButtonEventType;
+import de.abas.erp.axi2.type.ScreenEventType;
+import de.abas.erp.axi2.annotation.ScreenEventHandler;
+import de.abas.erp.axi2.event.ScreenEvent;
+
+@EventHandler(head = ControlVarnameList.class, row = ControlVarnameList.Row.class)
+@RunFopWith(EventHandlerRunner.class)
+public class ControlVarnameListEventHandler {
+
+	@ScreenEventHandler(type = ScreenEventType.ENTER)
+	public void screenEnter(ScreenEvent event, ScreenControl screenControl, 
+			DbContext ctx, ControlVarnameList head) throws EventException{
+		screenControl.setInvisible(head, ControlVarnameList.META.yheadfields, 
+			true);
+		screenControl.setInvisible(head, ControlVarnameList.META.ytabfields, 
+			true);
+	}
+	
+	
+	@ButtonEventHandler(field="start", type = ButtonEventType.AFTER)
+	public void startAfter(ButtonEvent event, ScreenControl screenControl, 
+			DbContext ctx, ControlVarnameList head) throws EventException{
+		Vartab yvartab = head.getYvartab();
+		head.table().clear();
+		if (yvartab == null) {
+			throw new EventException("no vartab selected", 1);
+		}
+		
+		Iterable<Row> rows = yvartab.table().getRows();
+		for (Row row : rows) {
+			if (row.isInTab()) {
+				if (head.getYtabfields() == true) {
+					de.abas.erp.db.infosystem.custom.ow1.ControlVarnameList.Row appendRow = 
+						head.table().appendRow();
+					appendRow.setYtengname(row.getVarNameEnglish());
+					appendRow.setYtgername(row.getVarName());
+					appendRow.setYtistabfield(row.getInTab());
+				}
+			}else {
+				if (head.getYheadfields() == true) {
+					de.abas.erp.db.infosystem.custom.ow1.ControlVarnameList.Row appendRow = 
+						head.table().appendRow();
+					appendRow.setYtengname(row.getVarNameEnglish());
+					appendRow.setYtgername(row.getVarName());					
+				}
+			}
+		}
+	}
+
+
+	@FieldEventHandler(field="yvartab", type = FieldEventType.EXIT)
+	public void yvartabExit(FieldEvent event, ScreenControl screenControl, 
+			DbContext ctx, ControlVarnameList head) throws EventException{
+		Vartab yvartab = head.getYvartab();
+		head.setYvartabtext(yvartab);
+		head.table().clear();
+		head.setYheadfields(true);
+		screenControl.setInvisible(head, ControlVarnameList.META.yheadfields, false);
+		if (yvartab.isGrpWithTab()) {
+			screenControl.setInvisible(head, ControlVarnameList.META.ytabfields, false);
+			head.setYtabfields(true);
+		}else {
+			screenControl.setInvisible(head, ControlVarnameList.META.ytabfields, true);
+			head.setYtabfields(false);
+		}
+	}
+
+
+	@FieldEventHandler(field="yvartabtext", type = FieldEventType.EXIT)
+	public void yvartabtextExit(FieldEvent event, ScreenControl screenControl, 
+			DbContext ctx, ControlVarnameList head) throws EventException{
+		Vartab yvartab = head.getYvartabtext();
+		head.setYvartab(yvartab);		
+		head.table().clear();		
+		head.setYheadfields(true);
+		screenControl.setInvisible(head, ControlVarnameList.META.yheadfields, false);
+		if (yvartab.isGrpWithTab()) {
+			screenControl.setInvisible(head, ControlVarnameList.META.ytabfields, false);
+			head.setYtabfields(true);
+		}else {
+			screenControl.setInvisible(head, ControlVarnameList.META.ytabfields, true);
+			head.setYtabfields(false);
+		}
+	}
+}
